d145e03975df8aaf728cf634b2bae328e973f8e1
lky19961108 <41745544+lky19961108@users.noreply.github.com>
2020-03-03 22:39:55 +0800

    Update conf.py

--- conf.py
+++ conf.py
@@ -14,8 +14,8 @@ template = {
     "path": "../Galileo"
 }
 enable_jsdelivr = {
-    "enabled": False,
-    "repo": ""
+    "enabled": True,
+    "repo": "<lky19961108>/Blog-With-GitHub-Boilerplate@gh-pages"
 }
 
 # 站点设置
